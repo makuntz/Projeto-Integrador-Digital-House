87e9749dabdf135e71ea0708426a17646cf69211
rcthomazelli <rcthomazelli@gmail.com>
2022-02-26 19:30:47 -0300

    PartialHeaderFooter,EJS+CSS,AddNasPgs

---
+++ controller/indexController.js
@@ -0,0 +1,22 @@
+const home = require ('../package.json')
+
+const indexController = {
+    home: (req, res) => {
+        res.render('pg-home')
+    },
+
+    produtos: (req, res) => {
+        res.render('pg-produtos')
+    },
+
+    carrinho: (req, res) => {
+        res.render('pg-carrinho')
+    },
+
+    pagamento: (req, res) => {
+        res.render('pg-pagamento')
+    }
+
+}
+
+module.exports = indexController
